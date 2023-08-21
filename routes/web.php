cdef014c280df8c9e8f4a6ee2905044ccbd013c9
Marlon Tavares <marlontf@gmail.com>
2023-08-21 14:27:44 -0300

    List Plans Created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PlanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 Route::get('/', function () {
     return view('welcome');
