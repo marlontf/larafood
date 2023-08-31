1aba26fbe70d4e27cf6dbc1d285cf928c3d74d19
Marlon Tavares <marlontf@gmail.com>
2023-08-31 13:59:34 -0300

    Group web routes

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::put('admin/plans/{url}', [PlanController::class, 'update'])->name('plans.update');
-Route::get('admin/plans/{url}/edit', [PlanController::class, 'edit'])->name('plans.edit');
-Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
-Route::get('admin/plans/create', [PlanController::class, 'create'])->name('plans.create');
-Route::post('admin/plans', [PlanController::class, 'store'])->name('plans.store');
-Route::any('admin/plans/search', [PlanController::class, 'search'])->name('plans.search');
-Route::get('admin/plans/{url}', [PlanController::class, 'show'])->name('plans.show');
-Route::delete('admin/plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
+Route::prefix('admin')->group(function () {
+    Route::put('plans/{url}', [PlanController::class, 'update'])->name('plans.update');
+    Route::get('plans/{url}/edit', [PlanController::class, 'edit'])->name('plans.edit');
+    Route::get('plans', [PlanController::class, 'index'])->name('plans.index');
+    Route::get('plans/create', [PlanController::class, 'create'])->name('plans.create');
+    Route::post('plans', [PlanController::class, 'store'])->name('plans.store');
+    Route::any('plans/search', [PlanController::class, 'search'])->name('plans.search');
+    Route::get('plans/{url}', [PlanController::class, 'show'])->name('plans.show');
+    Route::delete('plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
+    Route::get('/', [PlanController::class, 'index'])->name('admin.index');
+});
 
-Route::get('admin', [PlanController::class, 'index'])->name('admin.index');
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
